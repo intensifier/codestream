fc5c9903f6b60f66c52865d5cca473e8d22ae82d
Calvin Allen <calvinallen@newrelic.com>
2024-05-30 18:27:40 -0400

    Update version for VS

--- vs/src/AssemblyInfo.cs
+++ vs/src/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("15.8.1")]
-[assembly: AssemblyFileVersion("15.8.1")]
+[assembly: AssemblyVersion("15.9.0")]
+[assembly: AssemblyFileVersion("15.9.0")]
