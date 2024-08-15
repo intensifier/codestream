3989ca8f40ab33312f32887bf390262323676b5e
TeamCity Build Agent <tca-10.30.9.108@cdstrm.dev>
2024-08-15 18:47:29 +0000

    Auto bump version on develop to 15.14.1 for next release

--- vs/src/AssemblyInfo.cs
+++ vs/src/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("15.14.0")]
-[assembly: AssemblyFileVersion("15.14.0")]
+[assembly: AssemblyVersion("15.14.1")]
+[assembly: AssemblyFileVersion("15.14.1")]
